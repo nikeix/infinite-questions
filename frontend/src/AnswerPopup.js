51b9b2880256782ea5a4c02af866f9496d01cfcc
Shahar Rose <rose.shahar@gmail.com>
2024-01-19 19:54:48 +0200

    Add wiki url

--- frontend/src/AnswerPopup.js
+++ frontend/src/AnswerPopup.js
@@ -1,30 +1,31 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-function AnswerPopup({ answer, onCorrect }) {
-  const [backgroundColor, setBackgroundColor] = useState('rgb(30, 184, 255)'); // Default background color
+function AnswerPopup({answer, source_url, onCorrect}) {
+    const [backgroundColor, setBackgroundColor] = useState('rgb(30, 184, 255)'); // Default background color
 
-  const handleCorrect = (correct) => {
-    // Change the background color based on correctness
-    if (correct) {
-      setBackgroundColor('green'); // Change to green for correct
-    } else {
-      setBackgroundColor('red'); // Change to red for incorrect
-    }
+    const handleCorrect = (correct) => {
+        // Change the background color based on correctness
+        if (correct) {
+            setBackgroundColor('green'); // Change to green for correct
+        } else {
+            setBackgroundColor('red'); // Change to red for incorrect
+        }
 
-    onCorrect(correct);
-  };
+        onCorrect(correct);
+    };
 
-  return (
-    <div className="answerPopup" style={{ backgroundColor }}>
-      <div className="answer">{answer}</div>
-      <div className="divider"></div>
-      <div className="correctButtonsText">האם צדקת?</div>
-      <div className="correctButtons">
-        <button className="correctButton" onClick={() => handleCorrect(false)}>X</button>
-        <button className="correctButton" onClick={() => handleCorrect(true)}>V</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="answerPopup" style={{backgroundColor}}>
+            <div className="answer">{answer}</div>
+            <a className="source_url" href={source_url}>ויקיפדיה</a>
+            <div className="divider"></div>
+            <div className="correctButtonsText">האם צדקת?</div>
+            <div className="correctButtons">
+                <button className="correctButton" onClick={() => handleCorrect(false)}>X</button>
+                <button className="correctButton" onClick={() => handleCorrect(true)}>V</button>
+            </div>
+        </div>
+    );
 }
 
 export default AnswerPopup;
