2811159678bb3edea50c5c2a4f71a99961e92983
Shahar Rose <rose.shahar@gmail.com>
2024-01-20 14:15:05 +0200

    api key for gateway with cookie

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,35 @@
 import asyncio
+import os
 from dataclasses import dataclass
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Security, HTTPException
+from fastapi.security import APIKeyHeader
+from starlette.status import HTTP_401_UNAUTHORIZED
 from yarl import URL
 
 from trivia_ai import generate_trivia_question
 from wikipedia_api import get_articles, TriviaType
 
 app = FastAPI()
+API_KEY_HEADER = APIKeyHeader(name="X-API-Key")
 
 ARTICLES_LIMIT = 10
+
 
 @dataclass
 class Trivia:
     question: str
     answer: str
     source_url: str
+
+
+def get_api_key(api_key_header: str = Security(API_KEY_HEADER)) -> str:
+    if api_key_header == os.environ.get("SERVER_SECRET"):
+        return api_key_header
+    raise HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
 
 
 async def generate_one_trivia(page_url: URL, summary: str) -> Trivia:
@@ -25,9 +39,10 @@ async def generate_one_trivia(page_url: URL, summary: str) -> Trivia:
 
 @app.get('/trivia')
 async def get_trivia_api(
-    max_articles: int = 1,
-    trivia_type: TriviaType = TriviaType.RANDOM,
-    ) -> list[Trivia]:
+        max_articles: int = 1,
+        trivia_type: TriviaType = TriviaType.RANDOM,
+        api_key: str = Security(get_api_key),
+) -> list[Trivia]:
     if max_articles > ARTICLES_LIMIT:
         return {'error': f'Maximum {ARTICLES_LIMIT} articles allowed'}
 
@@ -36,6 +51,7 @@ async def get_trivia_api(
     results = list(await asyncio.gather(*[generate_one_trivia(*r) for r in articles]))
 
     return results
+
 
 def main():
     import uvicorn
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,19 @@ import FinalScreen from './FinalScreen';
 import LoadingBar from "react-top-loading-bar";
 import {FidgetSpinner} from "react-loader-spinner"; // Import the FinalScreen component
 import Picker from 'react-mobile-picker'
+import Cookies from "universal-cookie";
 
 const triviaGenerationParams = {
   type: ['RANDOM', 'NEWS'],
 }
 
-async function fetchQuestions(triviaParams) {
+async function fetchQuestions(triviaParams, server_secret) {
   console.log(triviaParams.type)
-  const response = await fetch(`/api/trivia?max_articles=3&trivia_type=${triviaParams.type}`);
+  const response = await fetch(`/api/trivia?max_articles=3&trivia_type=${triviaParams.type}`, {
+    headers: new Headers({
+      'X-API-Key': server_secret
+    })
+  });
   const trivia = await response.json();
   return trivia;
   // return [{"question":"שאלה: מי הוא איש העסקים שבשליטתו של חברת דלק נדל\"ן?","answer":"תשובה: יצחק תשובה.","source_url":"https://he.wikipedia.org/wiki/%D7%93%D7%9C%D7%A7_%D7%A0%D7%93%D7%9C%22%D7%9F"},{"question":"שאלה: איזה תפקיד כיהן אודי שני בצה\"ל?","answer":"תשובה: ראש אגף התקשוב, מפקד הגיס הצפוני, ומנכ\"ל משרד הביטחון.","source_url":"https://he.wikipedia.org/wiki/%D7%90%D7%95%D7%93%D7%99_%D7%A9%D7%A0%D7%99"},{"question":"שאלה: מתי הוקמה שכונת זיכרון יוסף?","answer":"תשובה: בשנת 1927.","source_url":"https://he.wikipedia.org/wiki/%D7%96%D7%99%D7%9B%D7%A8%D7%95%D7%9F_%D7%99%D7%95%D7%A1%D7%A3"}];
@@ -28,13 +33,13 @@ function App() {
   const [triviaParams, setTriviaParams] = useState({
     type: 'RANDOM'
   })
+  const cookies = new Cookies(null, { path: '/' });
   // Question index to answer: True, False, Null
-
 
 
   const startQuiz = async () => {
     setIsLoadingQuestions(true);
-    const questions = await fetchQuestions(triviaParams);
+    const questions = await fetchQuestions(triviaParams, cookies.get('server_secret'));
     setIsLoadingQuestions(false);
 
     // add userAnswer property to each question
